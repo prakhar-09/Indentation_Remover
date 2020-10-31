4d97642c00b0783441c7410790f76a30beb5e41e
Prakhar Saxena <prakhar.11911235@lpu.in>
2020-10-31 16:44:15 +0530

    Create wersal.py

---
+++ wersal.py
@@ -0,0 +1,4 @@
+ if "good bye" in statement or "ok bye" in statement or "stop" in statement or "bye" in statement:
+            speak('your personal assistant Prakhar is shutting down,Good bye')
+            print('your personal assistant Prakhar is shutting down,Good bye')
+            break
